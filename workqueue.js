45d396ae0a8fa28a66f298d3c2fabbfe7fc11ff4
Dusty Doris <dusty@doris.name>
2014-12-26 22:06:17 -0500

    catch SIGINT/SIGTERM and cleanly exit

--- workqueue.js
+++ workqueue.js
@@ -1,6 +1,6 @@
 /**
- * This class manages a list of Firebase elements and dispatches items in it to 
- * be processed. It is designed to only process one item at a time. 
+ * This class manages a list of Firebase elements and dispatches items in it to
+ * be processed. It is designed to only process one item at a time.
  *
  * It uses transactions to grab queue elements, so it's safe to run multiple
  * workers at the same time processing the same queue.
@@ -11,19 +11,34 @@
 function WorkQueue(queueRef, processingCallback) {
   this.processingCallback = processingCallback;
   this.busy = false;
+  this.shutdownRequested = false;
   queueRef.limitToFirst(1).on("child_added", function(snap) {
     this.currentItem = snap.ref();
     this.tryToProcess();
   }, this);
+  this.queueRef = queueRef;
 }
 
 WorkQueue.prototype.readyToProcess = function() {
   this.busy = false;
+  if (this.shutdownRequested) {
+    console.log('Finished processing. Shutting down.');
+    process.exit();
+  }
   this.tryToProcess();
 }
 
+WorkQueue.prototype.shutdown = function() {
+  this.queueRef.off();
+  this.shutdownRequested = true;
+  if (!this.busy) {
+    console.log('Not busy. Shutting down.');
+    process.exit();
+  }
+}
+
 WorkQueue.prototype.tryToProcess = function() {
-  if(!this.busy && this.currentItem) {
+  if (!this.busy && this.currentItem) {
     this.busy = true;
     var dataToProcess = null;
     var self = this;
@@ -31,22 +46,22 @@ WorkQueue.prototype.tryToProcess = function() {
     this.currentItem = null;
     toProcess.transaction(function(theItem) {
       dataToProcess = theItem;
-      if(theItem) {
+      if (theItem) {
         return null;
       } else {
         return;
       }
     }, function(error, committed, snapshot, dummy) {
-       if (error) throw error;
-       if(committed) {
-         console.log("Claimed a job.");
-	 self.processingCallback(dataToProcess, function() {
-	   self.readyToProcess();
-	 });
-       } else {
-         console.log("Another worker beat me to the job.");
-	 self.readyToProcess();
-       }
+      if (error) throw error;
+      if (committed) {
+        console.log("Claimed a job.");
+        self.processingCallback(dataToProcess, function() {
+          self.readyToProcess();
+        });
+      } else {
+        console.log("Another worker beat me to the job.");
+        self.readyToProcess();
+      }
     });
   }
 }
